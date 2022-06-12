a83f9933bb71653d51feeba85d8ab43bc817159f
Laudi <aleckoles@gmail.com>
2022-06-13 00:01:04 +0300

    first

--- script.js
+++ script.js
@@ -55,3 +55,59 @@ client.getBalance();
 client.withdraw(40000);
 client.withdraw(80000);
 client.relif(50000);
+
+const calculator = {
+    firstNumber: 0,
+    secondNumber: 0,
+    value: 0,
+    calcSumm() {
+        this.firstNumber = prompt('Enter first number');
+        this.secondNumber = prompt('Enter second number');
+        let summa = +this.firstNumber + +this.secondNumber;
+        console.log(`FirstNumber: ${this.firstNumber}; SecondNumber: ${this.secondNumber}; Summ = ${summa}`);
+    },
+    calcDiff() {
+        this.firstNumber = prompt('Enter first number');
+        this.secondNumber = prompt('Enter second number');
+        let diff = this.firstNumber - this.secondNumber;
+        console.log(`FirstNumber: ${this.firstNumber}; SecondNumber: ${this.secondNumber}; Difference = ${diff} `);
+    },
+    calcMult() {
+        this.firstNumber = prompt('Enter first number');
+        this.secondNumber = prompt('Enter second number');
+        let mult = this.firstNumber * this.secondNumber;
+        console.log(`FirstNumber: ${this.firstNumber}; SecondNumber: ${this.secondNumber}; Product = ${mult} `);
+    },
+    calcDivis() {
+        this.firstNumber = prompt('Enter first number');
+        this.secondNumber = prompt('Enter second number');
+        let divis = this.firstNumber / this.secondNumber;
+        console.log(`FirstNumber: ${this.firstNumber}; SecondNumber: ${this.secondNumber}; Division = ${divis} `);
+    },
+    memAdd() {
+        this.value = +prompt(`Enter the number in to memory`);
+        console.log(`In memory: ${this.value}`);
+    },
+    memClear() {
+        this.value = 0;
+        console.log(`In memory: ${this.value}`);
+    },
+    numberAddMem(number) {
+        this.value += +number;
+        console.log(`In memory: ${this.value}`);
+    },
+    memRead() {
+        console.log(`In memory: ${this.value}`);
+    }
+
+}
+//console.log(calculator);
+//calculator.calcSumm();
+//calculator.calcDiff();
+//calculator.calcMult();
+//calculator.calcDivis();
+calculator.memAdd();
+calculator.numberAddMem(11);
+calculator.memRead();
+//calculator.memClear();
+//console.log(calculator);
